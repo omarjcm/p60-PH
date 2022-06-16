59a6cf912de913fe900d97e11d993ffe34ee494d
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2022-06-15 19:31:04 -0500

    Javascript - Mouse movimiento.

--- code/javascript-dom/taller_08/script.js
+++ code/javascript-dom/taller_08/script.js
@@ -5,8 +5,20 @@ function presentar_opcion(opcion) {
     if (opcion == 1) {
         opcion_1.style.zIndex = 2
         opcion_2.style.zIndex = 1
+        mover_izquierda(opcion_2)
+        mover_izquierda(opcion_2)
+        
     } else if (opcion == 2) {
         opcion_1.style.zIndex = 1
         opcion_2.style.zIndex = 2
     }
+}
+
+function mover_izquierda(capa) {
+    paso = 10
+    setTimeout(function(){
+        let valor = parseInt(capa.offsetLeft)
+        valor += paso
+        capa.style.left = valor + 'px'
+    }, 3000)
 }
---
+++ code/javascript-dom/taller_09/script.js
@@ -0,0 +1,29 @@
+var div_cajita = document.getElementById('cajita')
+var es_presionada = false
+var distancia = [0, 0]
+
+div_cajita.addEventListener('mousedown', function(e){
+    es_presionada = true
+
+    let cajita_top = div_cajita.offsetTop
+    let cajita_left = div_cajita.offsetLeft
+    let mouse_x = e.clientX
+    let mouse_y = e.clientY
+
+    distancia = [
+        this.offsetLeft - e.clientX,
+        this.offsetTop - e.clientY,
+    ]
+
+})
+
+div_cajita.addEventListener('mousemove', function(e){
+    if (es_presionada) {
+        div_cajita.style.left = (e.clientX + distancia[0]) + 'px'
+        div_cajita.style.top = (e.clientY + distancia[1]) + 'px'
+    }   
+})
+
+div_cajita.addEventListener('mouseup', function(e){
+    es_presionada = false
+})
