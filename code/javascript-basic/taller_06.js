8eb23af5d300d564960dc404939090f92bb4ef03
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2022-06-22 19:34:00 -0500

    Arreglos

---
+++ code/javascript-basic/taller_06.js
@@ -0,0 +1,18 @@
+var estudiantes = [
+    'Alejandro Barreiro',
+    'Alexis Moran',
+    'Antony Pesantes',
+    'Bryan Mendoza',
+    'Eliseo Salazar',
+    'Dyllan Tumaille',
+    'Caviedes Vicente',
+    'Evelyn Manzaba',
+]
+
+for (let estudiante of estudiantes) {
+    console.log(estudiante)
+}
+
+for (let i=0; i<estudiantes.length; i++) {
+    console.log(estudiantes[i])
+}
