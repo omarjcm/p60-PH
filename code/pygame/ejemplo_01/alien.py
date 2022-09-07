af58e8d148eec89413667dcaa2000d64bc79eb97
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2022-09-06 21:44:23 -0500

    Implementacion de las condiciones de ganar o perder.

--- code/pygame/ejemplo_01/alien.py
+++ code/pygame/ejemplo_01/alien.py
@@ -9,12 +9,12 @@ class Alien:
 
     def dibujar(self):
         pg.draw.rect(self.juegos.ventana, (210, 250, 251), pg.Rect(self.x, self.y, self.tamanio, self.tamanio))
-        self.y += 0.05
+        self.y += 0.1
 
     def verificar_colision(self):
         for rayo in self.juegos.rayos:
             if (rayo.x < self.x+self.tamanio and rayo.x > self.x-self.tamanio and
                 rayo.y < self.y+self.tamanio and rayo.y > self.y-self.tamanio):
-                #self.juegos.rayos.remove(rayo)
+                self.juegos.rayos.remove(rayo)
                 self.juegos.aliens.remove(self)
 
--- code/pygame/ejemplo_01/juego.py
+++ code/pygame/ejemplo_01/juego.py
@@ -12,12 +12,15 @@ class Juego:
         self.ancho = ancho
         self.alto = alto
         self.color = color
+        self.margen = 20
 
         self.rayos = []
 
         self.aliens = []
         self.num_aliens_horizontal = 50
         self.num_aliens_vertical = 30
+
+        self.perdiste = False
 
         self.ventana = pg.display.set_mode( (self.ancho, self.alto) )
         self.reloj = pg.time.Clock()
@@ -29,20 +32,21 @@ class Juego:
 
         while not self.jugando:
             if len(self.aliens) == 0:
-                self.mensaje_ganador()
+                self.mostrar_mensaje('Ganaste!')
                 
             self.teclado = pg.key.get_pressed()
 
             if self.teclado[pg.K_LEFT]:
-                self.nave.x -= 2
+                self.nave.x -= 2 if self.nave.x > self.margen else 0
             elif self.teclado[pg.K_RIGHT]:
-                self.nave.x += 2
+                self.nave.x += 2 if self.nave.x < (self.ancho - self.margen) else 0
 
             for evento in pg.event.get():
                 if evento.type == pg.QUIT:
                     self.jugando = True
                 if evento.type == pg.KEYDOWN and evento.key == pg.K_SPACE:
-                    self.rayos.append( Rayo(self.ventana, self.nave.x, self.nave.y) )
+                    if not self.perdiste:
+                        self.rayos.append( Rayo(self.ventana, self.nave.x, self.nave.y) )
 
             pg.display.flip()
             self.reloj.tick(60)
@@ -55,17 +59,22 @@ class Juego:
                 alien.dibujar()
                 alien.verificar_colision()
 
-            self.nave.dibujar()
+                if alien.y > self.alto-self.margen:
+                    self.perdiste = True
+                    self.mostrar_mensaje('Perdiste!')
 
-    def mensaje_ganador(self):
+            if not self.perdiste:
+                self.nave.dibujar()
+
+
+    def mostrar_mensaje(self, mensaje):
         pg.font.init()
         fuente = pg.font.SysFont('Arial', 50)
-        texto = fuente.render('Ganaste!!', False, (250, 250, 250))
+        texto = fuente.render(mensaje, False, (250, 250, 250))
         self.ventana.blit(texto, (110, 160))
 
     def generar_aliens(self):
-        margen = 20
         tamanio_alien = 20
-        for x in range(margen, self.ancho-margen, tamanio_alien+5):
-            for y in range(margen, int(self.alto/2), tamanio_alien+5):
+        for x in range(self.margen, self.ancho-self.margen, tamanio_alien+5):
+            for y in range(self.margen, int(self.alto/2), tamanio_alien+5):
                 self.aliens.append( Alien(self, x, y, tamanio_alien) )
